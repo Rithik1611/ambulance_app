4943226f7381c1c78a1a65fd56e99d1cbb8262c2
kishore77s <skishore792004@gmail.com>
2024-08-01 21:16:11 +0530

    Created the Favourite page from alert

---
+++ client/lib/pages/alert_fav.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AlertFav extends StatefulWidget{
+  const AlertFav ({super.key});
+  
+  @override
+  State<AlertFav> createState() => _AlertFavState();
+}
+class _AlertFavState extends State<AlertFav>{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title:const Text('Favourite'),
+      backgroundColor: Colors.red,),
+      body: Stack(
+        children: [
+          // Your body content here,
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: const Icon(Icons.add),
+                backgroundColor: Colors.red,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
